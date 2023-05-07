ef0a1b040ab56b79844d21e7799599451e63e77e
Esad Hasanovic <56908978+Ehasanovic01@users.noreply.github.com>
2023-05-07 03:08:35 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,3 @@ document.addEventListener('DOMContentLoaded', function () {
   AOS.init();
 });
 
-
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  // Form validation and submission logic
-  alert('Your message has been sent! We will get back to you soon.');
-});

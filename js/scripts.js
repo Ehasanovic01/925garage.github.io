28a876d745721fbd6a5a9a8a83dd0fd605bbb8ef
Esad Hasanovic <56908978+Ehasanovic01@users.noreply.github.com>
2023-05-08 01:30:18 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  AOS.init();
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent, we will get back to you soon!');
 });
-
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  // Form validation and submission logic
-  alert('Your message has been sent! We will get back to you soon.');
-});
-
